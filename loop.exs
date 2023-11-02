cc8b32ba5f09a7b6316130db0273b4dc3e4cae45
Matheus de Gondra <matheusgondra951@gmail.com>
2023-11-02 09:03:02 -0300

    grouped functions

--- loop.exs
+++ loop.exs
@@ -1,8 +1,8 @@
 defmodule MeuModulo.Loop do
-  defp tabuada(list, _, 11), do: {:ok, Enum.reverse(list)}
 
   def tabuada(multiplicador), do: tabuada([], multiplicador, 1)
 
+  defp tabuada(list, _, 11), do: {:ok, Enum.reverse(list)}
   defp tabuada(lista, produto1, produto2) do
     IO.puts("#{produto1} x #{produto2} = #{produto1 * produto2}")
     lista = [produto1 * produto2 | lista]
